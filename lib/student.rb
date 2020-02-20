8f8821ccafd0cbf046b3cda199becccf378e6ea5
Rick Belson <belsonrj@aol.com>
2020-02-20 04:06:07 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,7 +2,11 @@ require_relative "./user.rb"
 class Student < User
   attr_accessor :knowledge
   
-  def knowledge(knowledge)
-    @knowledge = knowledge
+  def initialize
+    @knowledge = []
+  end
+
+  def learn(string)
+    knowledge << string
   end
 end
